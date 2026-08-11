91234cf708ceaa9843e8a8c8263c5bf6a61e2ed6
mohamad hadi <mhs498420@gmail.com>
2026-08-11 23:13:35 +0300

    feat: implement tasks page with status filtering, search functionality, and state handling

--- lib/src/view/pages/tasks_page.dart
+++ lib/src/view/pages/tasks_page.dart
@@ -1,15 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task_management/core/utils/enums.dart';
 import 'package:task_management/injection_container.dart';
 import 'package:task_management/src/logic/cubit/search_btn_cubit.dart';
 import 'package:task_management/src/logic/tasks/tascks_state.dart';
 import 'package:task_management/src/logic/tasks/tasks_bloc.dart';
 import 'package:task_management/src/model/task_model.dart';
 import 'package:task_management/src/view/pages/details_task_page.dart';
-import 'package:task_management/src/view/pages/edit_task_page.dart';
 import 'package:task_management/src/view/widgets/task_card.dart';
 import 'package:task_management/src/logic/tasks/tasks_event.dart';
-
 
 class TasksPage extends StatefulWidget {
   const TasksPage({Key? key}) : super(key: key);
@@ -20,17 +19,35 @@ class TasksPage extends StatefulWidget {
 
 class _TasksPageState extends State<TasksPage> {
   final TextEditingController _searchController = TextEditingController();
+  int _selectedTabIndex = 0;
 
   @override
   void initState() {
     super.initState();
-    // Load tasks when page initializes
+    _loadTasksForCurrentTab();
   }
 
   @override
   void dispose() {
     _searchController.dispose();
     super.dispose();
+  }
+
+  void _loadTasksForCurrentTab() {
+    switch (_selectedTabIndex) {
+      case 0:
+        sl<TasksBloc>().add(LoadTasks());
+        break;
+      case 1:
+        sl<TasksBloc>().add(FilterTasksBySpecificStatus(TaskStatus.todo));
+        break;
+      case 2:
+        sl<TasksBloc>().add(FilterTasksBySpecificStatus(TaskStatus.inProgress));
+        break;
+      case 3:
+        sl<TasksBloc>().add(FilterTasksBySpecificStatus(TaskStatus.done));
+        break;
+    }
   }
 
   @override
@@ -48,10 +65,15 @@ class _TasksPageState extends State<TasksPage> {
           },
         ),
 
+        // Status filter TabBar
+        _buildStatusTabBar(),
+
+        const SizedBox(height: 4),
+
         // Tasks list
         Expanded(
           child: BlocBuilder<TasksBloc, TasksState>(
-            bloc: sl<TasksBloc>()..add(LoadTasks()),
+            bloc: sl<TasksBloc>(),
             builder: (context, state) {
               if (state is TasksLoading) {
                 return const Center(
@@ -77,6 +99,79 @@ class _TasksPageState extends State<TasksPage> {
     );
   }
 
+  Widget _buildStatusTabBar() {
+    final tabs = [
+      {'title': 'الكل', 'icon': Icons.list_alt_rounded, 'color': const Color(0xFF4A90E2)},
+      {'title': 'يجب إنجازه', 'icon': Icons.radio_button_unchecked, 'color': const Color(0xFF4A90E2)},
+      {'title': 'قيد العمل', 'icon': Icons.sync, 'color': const Color(0xFFF39C12)},
+      {'title': 'تم إنجازها', 'icon': Icons.check_circle_outline, 'color': const Color(0xFF27AE60)},
+    ];
+
+    return Container(
+      height: 44,
+      margin: const EdgeInsets.symmetric(horizontal: 12, vertical: 6),
+      child: ListView.separated(
+        scrollDirection: Axis.horizontal,
+        itemCount: tabs.length,
+        separatorBuilder: (_, __) => const SizedBox(width: 8),
+        itemBuilder: (context, index) {
+          final isSelected = _selectedTabIndex == index;
+          final tab = tabs[index];
+          final Color accentColor = tab['color'] as Color;
+
+          return GestureDetector(
+            onTap: () {
+              setState(() {
+                _selectedTabIndex = index;
+              });
+              _loadTasksForCurrentTab();
+            },
+            child: AnimatedContainer(
+              duration: const Duration(milliseconds: 200),
+              padding: const EdgeInsets.symmetric(horizontal: 14, vertical: 8),
+              decoration: BoxDecoration(
+                color: isSelected ? accentColor : const Color(0xFF2A2D3E),
+                borderRadius: BorderRadius.circular(20),
+                boxShadow: isSelected
+                    ? [
+                        BoxShadow(
+                          color: accentColor.withOpacity(0.35),
+                          blurRadius: 6,
+                          offset: const Offset(0, 2),
+                        )
+                      ]
+                    : null,
+                border: Border.all(
+                  color: isSelected ? accentColor : Colors.white12,
+                  width: 1,
+                ),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  Icon(
+                    tab['icon'] as IconData,
+                    size: 16,
+                    color: isSelected ? Colors.white : Colors.grey[400],
+                  ),
+                  const SizedBox(width: 6),
+                  Text(
+                    tab['title'] as String,
+                    style: TextStyle(
+                      color: isSelected ? Colors.white : Colors.grey[300],
+                      fontSize: 13,
+                      fontWeight: isSelected ? FontWeight.bold : FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
   Widget _buildSearchBar() {
     return Container(
       margin: const EdgeInsets.all(16),
@@ -86,7 +181,7 @@ class _TasksPageState extends State<TasksPage> {
       ),
       child: TextField(
         controller: _searchController,
-        style: const TextStyle(color: Colors.black),
+        style: const TextStyle(color: Colors.white),
         decoration: const InputDecoration(
           hintText: 'البحث في المهام...',
           hintStyle: TextStyle(color: Colors.grey),
@@ -101,7 +196,7 @@ class _TasksPageState extends State<TasksPage> {
     );
   }
 
-  Widget _buildTasksList(List tasks) {
+  Widget _buildTasksList(List<TaskModel> tasks) {
     return ListView.builder(
       padding: const EdgeInsets.only(bottom: 100), // Space for FAB
       itemCount: tasks.length,
@@ -170,7 +265,7 @@ class _TasksPageState extends State<TasksPage> {
           const SizedBox(height: 16),
           ElevatedButton(
             onPressed: () {
-              sl<TasksBloc>().add(LoadTasks());
+              _loadTasksForCurrentTab();
             },
             style: ElevatedButton.styleFrom(
               backgroundColor: const Color(0xFF4A90E2),
@@ -186,12 +281,10 @@ class _TasksPageState extends State<TasksPage> {
     Navigator.push(
       context,
       MaterialPageRoute(
-        // builder: (context) => EditTaskPage(task: task),
         builder: (_) => DetailsTaskPage(task: task),
       ),
     ).then((_) {
-      // Refresh tasks after adding new task
-      sl<TasksBloc>().add(LoadTasks());
+      _loadTasksForCurrentTab();
     });
   }
 }
